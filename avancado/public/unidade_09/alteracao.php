1c02fc852eb9fd8a4a71795fa6a7dacc57a1a752
Tarcisio <caninosbrancos10031@gmail.com>
2022-08-17 22:32:11 -0300

    criar pag de alteracao

--- avancado/public/unidade_09/alteracao.php
+++ avancado/public/unidade_09/alteracao.php
@@ -1,8 +1,48 @@
 <?php require_once("../../conexao/conexao.php"); ?>
-
-
 <?php
 
+
+if(isset($_POST["cidade"])){
+
+    $nome = $_POST["nometransportadora"];
+    $endereco = $_POST["endereco"];
+    $cidade = $_POST["cidade"];
+    $estado = $_POST["estados"];
+    $cep = $_POST["cep"];
+    $cnpj = $_POST["cnpj"];
+    $telefone = $_POST["telefone"];
+    $transportadora = $_POST["transportadoraID"];
+
+
+    // Objeto de alteração
+    $alterar   = "UPDATE transportadoras ";
+    $alterar   .= " SET ";
+    $alterar   .= " nometransportadora = '{$nome}', ";
+    $alterar   .= " endereco = '{$endereco}', ";
+    $alterar   .= " cidade = '{$cidade}', ";
+    $alterar   .= " estadoID = {$estado}, ";
+    $alterar   .= " cep = '{$cep}', ";
+    $alterar   .= " cnpj = '{$cnpj}', ";
+    $alterar   .= " telefone = '{$telefone}' ";
+
+    $alterar   .= " WHERE transportadoraID = {$transportadora} ";
+
+    $operacao_alteracao = mysqli_query($conecta, $alterar);
+    if(!$operacao_alteracao){
+        die("error no banco");
+    }else{
+        header("location:listagem.php");
+    }
+
+}
+
+
+
+
+
+
+
+// consulta a tabela de transportadora
 $transp = " SELECT * FROM transportadoras ";
 
 if(isset($_GET["codigo"])){
@@ -20,7 +60,10 @@ if(!$con_transp){
     $info_transp = mysqli_fetch_assoc($con_transp);
 }
 
-
+// consulta a tabela de estados
+$estados = "SELECT estadoID, nome FROM estados ";
+$linha_estados = mysqli_query($conecta, $estados);
+if(!$linha_estados){ die("falha no banco");}
 ?>
 
 <!doctype html>
@@ -58,10 +101,44 @@ if(!$con_transp){
                 <input type="text" value=" <?php echo $info_transp["cidade"]; ?>" name="cidade">
                     <!--  -->
                 
+
+
+
+
+
+
                 <label for="estados"> Estados </label>
+
                 <select name="estados" id="estados">
-                    <!-- <option value="1">tarcisio</option> -->
-                </select>
+                              
+                    <?php
+                    $meuestado = $info_transp["estadoID"];
+                     while($linha = mysqli_fetch_assoc($linha_estados)){ 
+
+                        $estado_momento = $linha["estadoID"];
+                        if($meuestado == $estado_momento){
+                            
+                            ?>
+
+                    <option value="<?php echo $linha["estadoID"]; ?>" selected >
+                        <?php echo $linha["nome"]; ?>
+                    </option>
+
+                    
+                    
+                    <?php  }else{  ?>                                 
+                        
+                        
+                    <option value="<?php echo $linha["estadoID"]; ?> ">
+                        <?php echo $linha["nome"]; ?>
+                    </option>
+                    
+                    
+                    <?php } } ?>
+</select>
+
+
+
                     <!--  -->
                 
                 <label for="cep"> Cep </label>
@@ -80,9 +157,6 @@ if(!$con_transp){
                                     <!--  -->
                 
                 <input type="submit" name="" value="Confirmar Alteração">
-   
-
-
                 
                 </form>
             </div>
